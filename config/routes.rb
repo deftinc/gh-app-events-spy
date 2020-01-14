e98b94b48aece388bb662ec4e932367d270c0ff5
Patrick Wiseman <patrick@deft.services>
2020-01-14 15:29:48 -0500

    Setup route to default back to the heroku dashboard to get to kibana

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  post '/webhooks/github' => 'github_webhooks#handle'
+  root to: redirect { |path_params, req| "https://dashboard.heroku.com/apps/#{req.subdomain}" }
 end
