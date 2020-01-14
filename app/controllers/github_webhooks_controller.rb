093ba713aaf99037065ecddec4945a47c2caa20e
Patrick Wiseman <patrick@deft.services>
2020-01-14 16:07:29 -0500

    Get rid of CSRF for webhooks, add signature check

--- app/controllers/github_webhooks_controller.rb
+++ app/controllers/github_webhooks_controller.rb
@@ -1,8 +1,12 @@
 require 'elasticsearch'
 
 class GithubWebhooksController < ApplicationController
+  skip_before_action :verify_authenticity_token
+  before_action -> { head :unauthorized unless Rack::Utils.secure_compare(signature, expected_signature) }
+
   def handle
     client.index(index: 'github_events', body: payload)
+    render plain: 'Thanks, GitHub!'
   end
 
   private
@@ -18,5 +22,13 @@ class GithubWebhooksController < ApplicationController
 
   def client
     @client ||= Elasticsearch::Client.new(host: ENV.fetch('BONSAI_URL'), log: true)
+  end
+
+  def signature
+    @signature ||= request.headers['X-Hub-Signature']
+  end
+
+  def expected_signature
+    @expected_signature ||= "sha1=#{OpenSSL::HMAC.hexdigest('sha1', ENV['GITHUB_WEBHOOK_SECRET'], request.raw_post)}"
   end
 end
